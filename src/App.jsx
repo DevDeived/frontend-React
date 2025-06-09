c1b259c7d4aff694bbce4b369fc72c05b70a7a55
EliasFernandes02 <eliasneto0014@gmail.com>
2025-06-09 20:42:05 -0300

    Pega acai

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Banner from "./Components/Banner"
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Banner/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
