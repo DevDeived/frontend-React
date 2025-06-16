3214a30cb86c5cea87b929e7f6a47cca32ab3bba
EliasFernandes02 <eliasneto0014@gmail.com>
2025-06-16 20:13:53 -0300

    la vai 2

---
+++ src/Components/LoginBody/LoginBody.jsx
@@ -0,0 +1,28 @@
+
+function LoginBody() {
+
+  return (
+    <>
+      <section id="loginBody">
+        <div>
+          <div>
+            <h1>Acesse sua conta</h1>
+            <p>Novo cliente ? Então registre-se <a href="">aqui</a></p>
+          </div>
+          <div>
+            <label htmlFor="">Login *</label>
+            <input type="email" placeholder="Insira seu login ou email" />
+
+            <label htmlFor="">Senha *</label>
+            <input type="password" placeholder="Insira sua senha" />
+          </div>
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default LoginBody
