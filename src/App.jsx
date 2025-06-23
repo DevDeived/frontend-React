c0d58948da1714c2eaead9eff3af782ae5c020a2
unknown <andersonsilva2533@digitalcollege.internal>
2025-06-23 20:57:40 -0300

    criacaoconta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Loginpage from "./Pages/Loginpage";
+import CriacaoContaPage from "./Pages/CriarContaPage";
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<Loginpage />} />
+          <Route path="/CreateCount" element={<CriacaoContaPage />} />
       </Routes>
     </BrowserRouter>
     </>
---
+++ src/Pages/CriarContaPage.jsx
@@ -0,0 +1,20 @@
+import CriacaoConta from "../Components/CriacaoConta/CriacaoConta"
+import Footer from "../Components/Footer/Footer"
+
+import LoginHeader from "../Components/LoginHeader/LoginHeader"
+
+
+function CriacaoContaPage() {
+  
+  return (
+    <>
+        <LoginHeader></LoginHeader>
+        <CriacaoConta></CriacaoConta>
+        <Footer></Footer>
+    </>
+  )
+}
+
+export default CriacaoContaPage
+
+
