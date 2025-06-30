7b25a00acf85072c8f6f200e07836494bdd6f498
Gabriela Lisboa <gabinblisboa@gmail.com>
2025-06-29 23:57:16 -0300

    atualizacao

--- src/Components/ConfirmacaoDeCompra/ResumoDaCompra.jsx
+++ src/Components/ConfirmacaoDeCompra/ResumoDaCompra.jsx
@@ -1,54 +1,37 @@
-import Tennis from "../../assets/White-Sneakers.png"
-import "./ResumoDaCompra.css"
+import Tennis from "../../assets/White-Sneakers.png";
+import "./ResumoDaCompra.css";
 function ResumoDaCompra() {
   return (
     <>
-      <section id="resumocompra">
-        <div>
-          <p >RESUMO</p>
-        </div>
+      <section id="resumoCompra">
+        <p>RESUMO</p>
         <hr />
-        <div id="resumo">
+        <div id="produto">
           <div id="tennis">
             <img src={Tennis} alt="" />
           </div>
-            <p>Tênis Nike Revolution 6 Next Nature Masculino </p>
-        
+          <p>Tênis Nike Revolution 6 Next Nature Masculino </p>
         </div>
         <hr />
-        <div>
-          <div>
+        <div id="resumo">
+          <div className="resumo">
             <p>Subtotal:</p>
-          </div>
-          <div>
             <p>R$ 219,00</p>
           </div>
-        </div>
-        <div>
-          <div>
+          <div class="resumo">
             <p>Frete:</p>
-          </div>
-          <div>
             <p>R$ 0,00</p>
           </div>
-        </div>
-        <div>
-          <div>
+          <div class="resumo">
             <p>Desconto:</p>
-          </div>
-          <div>
-            <p>R$ 0,00</p>
+            <p>R$ 30,00</p>
           </div>
         </div>
-        <div>
+        <div id="total">
           <div>
-          <div>
-            <p>TOTAL</p>
-          </div>
-          <div>
+            <p>Total</p>
             <p>R$ 219,00</p>
           </div>
-        </div>
           <div>
             <p>ou 10x de 21,90 sem juros</p>
           </div>
