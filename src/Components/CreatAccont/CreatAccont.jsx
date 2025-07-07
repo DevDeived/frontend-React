0329c90fe1e62daeb0bae59a8cced8916b0dda5d
EliasFernandes02 <eliasneto0014@gmail.com>
2025-07-07 20:57:39 -0300

    props

--- src/Components/CreatAccont/CreatAccont.jsx
+++ src/Components/CreatAccont/CreatAccont.jsx
@@ -1,71 +1,78 @@
 
 import "./CreatAccont.css"
-
+import Input from "../Input/Input";
 function CreatAccont() {
-  return (
-    <>
-      <section id="divigeral">
-        <div>
-        <div id="superiorcriar">
-                    <h1>Criar Conta</h1>
-                </div>
-            <div id="cabecalhosuperiorcreat">
-                <div class="campospreench">
-                    <p id="infope" >Informações Pessoais</p>
-                    <hr id="hrzinho"/>
-                    <label class="textsp" htmlFor="">Nome completo *</label>
-                    <input type="text" name="" placeholder="Insira seu nome" id="camposplace" />
-                </div>
-                <div class="campospreench">
-                    <label class="textsp" htmlFor="">CPF *</label>
-                    <input type="text" name="" placeholder="Insira seu CPF" id="camposplace" />
-                </div>
-                <div class="campospreench">
-                    <label class="textsp" htmlFor="">E-mail *</label>
-                    <input type="text" name="" placeholder="Insira seu email" id="camposplace" />
-                </div>
-                <div class="campospreench">
-                    <label class="textsp" htmlFor="">Celular *</label>
-                    <input type="text" name="" placeholder="Insira seu celular" id="camposplace" />
-                </div>                 
-            </div>
-        </div>
-        <div>
-            <div id="cabecalhosuperiorcreat">
-                <div id="novesfora">
-                    <p id="infoentr">Informações de Entrega</p>
-                    <hr />
-                    <label class="textsp" htmlFor="">Endereço *</label>
-                    <input type="text" name="" placeholder="Insira seu endereço" id="camposplace" />
-                </div>
-                <div class="campospreench">
-                    <label class="textsp" htmlFor="">Bairro *</label>
-                    <input type="text" name="" placeholder="Insira seu bairro" id="camposplace" />
-                </div>
-                <div class="campospreench">
-                    <label class="textsp" htmlFor="">Cidade *</label>
-                    <input type="text" name="" placeholder="Insira sua cidade" id="camposplace" />
-                </div>
-                <div class="campospreench">
-                    <label class="textsp" htmlFor="">CEP *</label>
-                    <input type="text" name="" placeholder="Insira seu CEP" id="camposplace" />
-                </div>
-                <div class="campospreench">
-                    <label class="textsp" htmlFor="">Complemento</label>
-                    <input type="text" name="" placeholder="Insira complemento" id="camposplace" />
-                </div>
-                <div id="checklembrete">
-                    <input type="checkbox" name="" id="checkin" />
-                    <label id="textocheck" htmlFor="">Quero receber por email ofertas e noviddades das lojas da Digital Store. A frequência de envios pode variar de acordo com a intenção do cliente.</label>
+    return (
+        <>
+            <section id="divigeral">
+                <div>
+                    <div id="superiorcriar">
+                        <h1>Criar Conta</h1>
+                    </div>
+                    <p className="infope" >Informações Pessoais</p>
+                    <hr className="hrzinho" />
+                    <div id="cabecalhosuperiorcreat">
+
+                        <Input
+                            placeholder="Insira seu nome"
+                            label="Nome Completo *"
+                            type="text"
+                        />
+                        <Input
+                            placeholder="Insira seu CPF"
+                            label="CPF *"
+                            type="text"
+                        />
+                        <Input
+                            placeholder="Insira seu email"
+                            label="E-mail *"
+                            type="email"
+                        />
+                        <Input
+                            placeholder="Insira seu celular"
+                            label="Celular *"
+                            type="text"
+                        />
+                    </div>
                 </div>
                 <div>
-                    <input type="button" id="botaodecriaraconta" value="Criar Conta" />
+                    <p className="infope">Informações de Entrega</p>
+                    <hr className="hrzinho" />
+                    <div id="cabecalhosuperiorcreat">
+
+                        <Input
+                            placeholder="Insira seu endereço"
+                            label="Endereço *"
+                            type="text"
+                        />
+                        <Input
+                            placeholder="Insira seu bairro"
+                            label="Bairro *"
+                            type="text"
+                        />
+                        <Input
+                            placeholder="Insira sua cidade"
+                            label="Cidade *"
+                            type="text"
+                        />
+                        <Input
+                            placeholder="Insira complemento"
+                            label="Complemento *"
+                            type="text"
+                        />
+
+                        <div id="checklembrete">
+                            <input type="checkbox" name="" id="checkin" />
+                            <label id="textocheck" htmlFor="">Quero receber por email ofertas e novidades das lojas da Digital Store. A frequência de envios pode variar de acordo com a intenção do cliente.</label>
+                        </div>
+                        <div>
+                            <input type="button" id="botaodecriaraconta" value="Criar Conta" />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-      </section>
-    </>
-  );
+            </section>
+        </>
+    );
 }
 
 export default CreatAccont;
---
+++ src/Components/Input/Input.jsx
@@ -0,0 +1,15 @@
+
+import "./Input.css"
+function Input(props) {
+
+  return (
+    <>
+        <div class="campospreench">
+            <label class="textsp" htmlFor="">{props.label}</label>
+            <input type={props.type} name="" placeholder={props.placeholder} id="camposplace" />
+        </div>
+    </>
+  )
+}
+
+export default Input
